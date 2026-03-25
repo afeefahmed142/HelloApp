8d312fb4baf506e706e4c01f6967b56d6e28c058
afeefahmed142 <afeefahmed142@gmail.com>
2026-03-25 13:59:34 +0530

    Coded UC5 using enhanced for loop

--- Hello.java
+++ Hello.java
@@ -1,12 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
-
-        // Default name
         String name = "World";
 
-        // Check if a name is provided as a command-line argument
+        
         if (args.length > 0) {
-            name = args[0]; // Use the provided name
+            name = args[0]; 
         }
 
         System.out.println("Hello, " + name + "!");
